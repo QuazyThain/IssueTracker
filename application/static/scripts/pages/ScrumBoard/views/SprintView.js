48b1de468d4f6191038cd59dc12e9a21aaaff3b1
re0ne <re0ne@ya.ru>
2014-03-25 14:36:11 +0200

    Render issue

--- application/static/scripts/pages/ScrumBoard/views/SprintView.js
+++ application/static/scripts/pages/ScrumBoard/views/SprintView.js
@@ -1,9 +1,10 @@
 define(["pages/ScrumBoard/Environment",
         "text!pages/ScrumBoard/html/SprintView.html",
         "css!pages/ScrumBoard/css/SprintView.css",
-        "pages/ScrumBoard/collections/Issues"],
+        "pages/ScrumBoard/collections/Issues",
+        "pages/ScrumBoard/views/IssueView"],
 
-    function(environment, sprintHTML, sprintCSS, Issues) {
+    function(environment, sprintHTML, sprintCSS, Issues, IssueView) {
         return Backbone.View.extend({
             id: "sprint",
 
@@ -21,9 +22,30 @@ define(["pages/ScrumBoard/Environment",
                 this.$el.html(this.template());
 
                 environment.issues.fetch({"reset": true});
+                environment.issues.on("reset", this.draw, this);
 
                 return this;
             },
+            
+            draw: function (collection, options) {
+                collection.each(this.issue, this);
+            },
+            
+            issue: function (issue) {
+                alert(issue.get("name"));
+                
+                var view = new IssueView({item: issue});
+                view.render();
+                
+                var selector = ".todo";
+                if (issue.get("status") == "doing") {
+                    selector = ".doing";
+                } else if (issue.get("status") == "") {
+                    selector = ".done";
+                }
+                
+                this.$(selector).append(view.el);
+            }
         });    
     }
 );
